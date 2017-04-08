26a47ebe39a117907a0735df742cb8788dfadb33
flupe <lucas@escot.me>
2017-04-08 23:57:47 +0000

    added main options

--- src/main.ml
+++ src/main.ml
@@ -2,43 +2,62 @@ open Ast
 open Print
 open Structures
 
-let _ =
+let parse_input () =
+  try
+    Lexing.from_channel stdin
+    |> Parser.main Lexer.token
+  with _ ->
+    print_endline <| magenta "Syntax Error";
+    exit 1
+
+let () =
+  let debug = ref false in
+  let machine = ref false in
+  let interm = ref "" in
+  let nbe = ref false in
+
+  let speclist =
+    [ "-debug", Arg.Set debug, "Display the parsed input."
+    ; "-machine", Arg.Set machine, "Compile and run the input from SECD."
+    ; "-interm", Arg.Set_string interm, "Compile input to the given output file."
+    ; "-NbE", Arg.Set nbe, "Run input with NbE."
+    ]
+
+  in Arg.parse speclist (fun x -> ()) "FOUINE INTERPRETOR 2017";
+
   let env = ref Env.empty in
   while true do
     print_string ">>> ";
     flush stdout;
 
-    let prog =
+    let prog = parse_input () in
+
+    if !debug then
+      print prog;
+
+    (* compile and run on SECD *)
+    if !machine then begin
+      let bytecode = Compiler.compile prog in
+      print_endline <| Bytecode.string_of_bytecode bytecode;
+
+      (* todo: handle interm *)
       try
-        Lexing.from_channel stdin
-        |> Parser.main Lexer.token
+        Secd.run bytecode |> Secd.print_value
       with _ ->
-        print_endline "syntax error";
-        exit 1;
-    in
-    print prog;
-
-    let bytecode =
-      Compiler.compile prog in
-
-    bytecode |> Bytecode.string_of_bytecode |> print_endline;
-
-    begin try
-      let error _ x =
-        print_endline (red "Uncaught exception E :");
-        Interpreter.print_result x
-      in
-      let success e x =
-        env := e;
-        Interpreter.print_result x
-      in
-      Interpreter.eval !env success error prog
-    with Interpreter.InterpretationError ->
-      print_endline "error while interpreting the program"
-    end;
-
-    try
-      Secd.run bytecode |> Secd.print_value
-    with _ ->
-      print_endline "error while running the program on the SECD";
+        print_endline "error while running the program on the SECD";
+    end
+    else begin
+      try
+        let error _ x =
+          print_endline (red "Uncaught exception E :");
+          Interpreter.print_result x
+        in
+        let success e x =
+          env := e;
+          Interpreter.print_result x
+        in
+        Interpreter.eval !env success error prog
+      with Interpreter.InterpretationError ->
+        print_endline "error while interpreting the program"
+    end
   done
